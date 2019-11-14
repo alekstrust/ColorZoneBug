1bada55cde27a99f0fc4932fd645fef458b65d51
Javier T <javier@tavara.pe>
2019-11-14 13:11:06 -0500

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using MaterialDesignThemes.Wpf;
+using System.Windows;
+
+namespace ColorZoneBug
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void txbCut_Loaded(object sender, RoutedEventArgs e)
+        {
+            ShadowAssist.SetShadowDepth(colorZone, ShadowDepth.Depth0);
+        }
+    }
+}
